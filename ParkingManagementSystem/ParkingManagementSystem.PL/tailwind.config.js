cb2bd4f27e26bdfeef0f14bd71112a9b08a7331f
Maxim Marinov <MMMarinov20@codingburgas.bg>
2023-12-21 12:30:47 +0200

    add tailwind css and HMR

---
+++ ParkingManagementSystem/ParkingManagementSystem.PL/tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './Pages/**/*.cshtml',  // Adjust the path based on your project structure
+  ],
+  // other configurations...
+  plugins: [
+    // your plugins here
+  ],
+}
