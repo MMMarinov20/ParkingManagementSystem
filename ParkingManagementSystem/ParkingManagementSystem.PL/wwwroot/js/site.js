41d22c2e2348fed14e55338ad1e54f656130bf2c
Maxim Marinov <MMMarinov20@codingburgas.bg>
2023-12-23 23:00:37 +0200

    add reservation modal

--- ParkingManagementSystem/ParkingManagementSystem.PL/wwwroot/js/site.js
+++ ParkingManagementSystem/ParkingManagementSystem.PL/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('reserve').addEventListener('click', function () {
+        document.getElementById('overlay').classList.remove('hidden');
+        document.getElementById('myModal').classList.remove('hidden');
+        document.body.style.overflow = 'hidden';
+    });
+
+    document.getElementById('closeModal').addEventListener('click', function () {
+        document.getElementById('overlay').classList.add('hidden');
+        document.getElementById('myModal').classList.add('hidden');
+        document.body.style.overflow = '';
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target === document.getElementById('overlay')) {
+            document.getElementById('overlay').classList.add('hidden');
+            document.getElementById('myModal').classList.add('hidden');
+            document.body.style.overflow = '';
+        }
+    });
+});
