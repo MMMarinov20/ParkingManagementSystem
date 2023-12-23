94cb2fb59ca515f48358ca2e4b6d0286a6bf7531
Maxim Marinov <MMMarinov20@codingburgas.bg>
2023-12-23 14:47:09 +0200

    add sample parking lots page

---
+++ ParkingManagementSystem/ParkingManagementSystem.PL/Pages/Lots.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ParkingManagementSystemPL.Pages
+{
+    public class LotsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
