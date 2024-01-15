bd1e287513172987d291b4b1b2e8cfc8f3579a93
Maxim Marinov <MMMarinov20@codingburgas.bg>
2024-01-15 12:06:38 +0200

    add .gitignore

--- ParkingManagementSystem/ParkingManagementSystem.Tests/obj/Debug/net7.0/ParkingManagementSystem.Tests.AssemblyInfo.cs
+++ ParkingManagementSystem/ParkingManagementSystem.Tests/obj/Debug/net7.0/ParkingManagementSystem.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParkingManagementSystem.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c33b5ca30e93de8a5a909cbf9e28c35cfb1098a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParkingManagementSystem.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParkingManagementSystem.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
